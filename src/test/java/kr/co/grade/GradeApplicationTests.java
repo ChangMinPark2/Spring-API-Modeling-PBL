5abd8e25aad54b4c98558899d2e5ba274012ff85
ChangMinPark2 <98qkrckdals@naver.com>
2024-03-05 00:36:43 +0900

    feat(Modeling) : 초기 틀

---
+++ src/test/java/kr/co/grade/GradeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.grade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
