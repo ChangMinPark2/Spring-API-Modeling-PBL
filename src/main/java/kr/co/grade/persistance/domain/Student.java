7911f705f07ed09478ae2fae15f6605534c3a616
ChangMinPark2 <98qkrckdals@naver.com>
2024-03-05 00:39:06 +0900

    feat(Modeling) : Student 도메인 생성

---
+++ src/main/java/kr/co/grade/persistance/domain/Student.java
@@ -0,0 +1,12 @@
+package kr.co.grade.persistance.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class Student {
+    private String name;
+    private int score;
+
+}
