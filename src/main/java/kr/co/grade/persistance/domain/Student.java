b50d9721d3bf0c7f48c07af6dceb8d4e22d9650c
ChangMinPark2 <98qkrckdals@naver.com>
2024-03-05 23:13:56 +0900

    refactor(Modeling) : Student 객체 수정

--- src/main/java/kr/co/grade/persistance/domain/Student.java
+++ src/main/java/kr/co/grade/persistance/domain/Student.java
@@ -1,17 +1,27 @@
 package kr.co.grade.persistance.domain;
 
-import lombok.Getter;
+import kr.co.grade.service.model.response.StudentResDto;
+import lombok.*;
 
 @Getter
+@Builder
+@AllArgsConstructor
 public class Student {
     private static Long idCounter = 0L;
     private final Long id;
     private final String name;
-    private final int score;
+    private final int grade;
 
-    public Student(String name, int score) {
+    public Student(String name, int grade) {
         this.id = ++idCounter;
         this.name = name;
-        this.score = score;
+        this.grade = grade;
+    }
+
+    public static StudentResDto.READ toReadAll(Student student) {
+        return StudentResDto.READ.builder()
+                .name(student.getName())
+                .grade(student.grade)
+                .build();
     }
 }
