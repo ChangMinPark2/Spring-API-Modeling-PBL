a1465418017b8e2241183d5d0bdeb771491c1448
ChangMinPark2 <98qkrckdals@naver.com>
2024-03-05 23:14:42 +0900

    feat(Modeling) : 응답/에러 모델 객체 생성

---
+++ src/main/java/kr/co/grade/infra/model/ApiResponse.java
@@ -0,0 +1,38 @@
+package kr.co.grade.infra.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Collections;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApiResponse<T> {
+    private Status status;
+    private MetaData metadata;
+    private List<T> results;
+
+    public static <T> ApiResponse<T> error(Status status) {
+        return ApiResponse.<T>builder()
+                .status(status)
+                .metadata(new MetaData(0)) // 빈 MetaData 객체로 초기화
+                .results(Collections.emptyList()) // 빈 리스트로 초기화
+                .build();
+    }
+
+    public static <T> ApiResponse<T> makeResponse(List<T> data) {
+        MetaData metadata = new MetaData(data.size());
+        Status status = new Status(ErrorCode.SUCCESS_VALUE);
+
+        return ApiResponse.<T>builder()
+                .status(status)
+                .metadata(metadata)
+                .results(data)
+                .build();
+    }
+}
